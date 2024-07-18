13823bff4a453cd626fca20c660b658648f4acc0
ArtJR2003 <163406569+ArtJR2003@users.noreply.github.com>
2024-07-18 17:27:55 +0400

    4.py

--- 4.py
+++ 4.py
@@ -1,6 +1,5 @@
 str = "reverse me"
-print(f"The original string: {str}")
-reversed_str1 = str[-1::-1]
-reversed_str2 = str[::-1]
-print(f"Reversed string: {reversed_str1}")
-print(f"Another reversed string: {reversed_str2}")
+reversed_str = ""
+for i in range(len(str) - 1, -1, -1):
+    reversed_str += str[i]
+print(reversed_str)
