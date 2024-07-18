95aa62fc6d91b90f0116027ab390483900359928
ArtJR2003 <163406569+ArtJR2003@users.noreply.github.com>
2024-07-18 10:42:25 +0400

    2.py

--- 2.py
+++ 2.py
@@ -1,6 +1,7 @@
-our_str = bytearray(b"capitalize")
-char = ord('c')
-for i in range(len(our_str)):
-	if char == our_str[i]:
-		our_str[i] = char - 32
-print(our_str.decode())
+str = bytearray(b"capitalize")
+target = ord('c')
+for i in range(len(str)):
+    if target == str[i]:
+        str[i] = target - 32
+        
+print(str.decode())
